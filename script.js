f1e8cdbc7ccd657aa29b00c7184b5a63b0c13f4e
Salman Ahmed <salmanahmd642@gmail.com>
2024-08-09 17:04:56 +0500

    git hub

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function navAnimation (){
-  var nav = document.querySelector("nav");
+var nav = document.querySelector(".nav-part-2");
 
 nav.addEventListener("mouseenter", function(){
   let tl = gsap.timeline()
@@ -39,3 +39,43 @@ nav.addEventListener("mouseleave", function(){
 })
 }
 navAnimation();
+
+function page2Animation(){
+  var rightEllums = document.querySelectorAll('.right-elem')
+
+rightEllums.forEach(function(elem){  
+  elem.addEventListener("mouseenter", function(){
+    gsap.to(elem.childNodes[3], {
+      opacity: 1,
+      scale: 1
+    })
+  })
+  elem.addEventListener("mouseleave", function(){
+    gsap.to(elem.childNodes[3], {
+      opacity: 0,
+      scale: 0
+    })
+  })
+  elem.addEventListener("mousemove", function(dets){
+    gsap.to(elem.childNodes[3], {
+      x:dets.x - elem.getBoundingClientRect().x-40,
+      y:dets.y - elem.getBoundingClientRect().y-125
+    })
+  })
+})
+}
+page2Animation();  
+
+function openWindow(){
+  document.addEventListener('DOMContentLoaded', function() {
+    const playIcon = document.getElementById('play-icon');
+    const video = document.getElementById('video-source');
+
+    playIcon.addEventListener('click', function() {
+      // Open the video URL in a new tab
+      window.open(video.src, '_blank');
+    });
+ });
+}
+
+openWindow(); 
